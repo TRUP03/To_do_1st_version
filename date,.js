b0dae109891641777e2d867fa05bb7a40fdc57bb
TRUP03 <truptee20_ug@ee.nits.ac.in>
2022-06-11 02:29:55 +0530

    two_pages_with_workList

---
+++ date,.js
@@ -0,0 +1,28 @@
+module.exports.getDate=getDate;
+
+function getDate()
+{
+    var date = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = date.toLocaleDateString('en-Us', options);
+    return day;
+}
+
+module.exports.getDay=getDay;
+
+function getDay() {
+    var date = new Date();
+
+    var options = {
+        weekday: "long",
+    };
+
+    var day = date.toLocaleDateString('en-Us', options);
+    return day;
+}
--- server.js
+++ server.js
@@ -2,32 +2,52 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const res = require('express/lib/response');
+const Date = require(__dirname + '/date,.js');
+
+
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-var items = [ "Eat Food", "Eat Fruits",  "Eat Dry Fruits"];
+var items = ["Eat Food", "Eat Fruits", "Eat Dry Fruits"];
+var workItems = [];
 
 app.get('/', (req, res) => {
-    var date = new Date();
-  
-    var options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
 
-    var day = date.toLocaleDateString('en-Us',options);
 
-    res.render('index',{kindOfDay : day, newItems : items });
+    var day = Date.getDay();
+   
+    res.render('index', {
+        listTitle: day,
+        newItems: items
+    });
 });
 
-app.post('/',(req,res)=>{
+app.post('/', (req, res) => {  
+    console.log(req.body);
     var item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+  
+    if (req.body.listT === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+});
+
+app.get('/work', (req, res) => {
+    res.render("index", {
+        listTitle: "Work List",
+        newItems: workItems
+    });
+});
+
+app.post('/work', (req, res) => {
+    res.redirect('/work');
 });
 
 app.listen(3000, () => {
